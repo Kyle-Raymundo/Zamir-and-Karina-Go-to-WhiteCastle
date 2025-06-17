37b69ff4fd468cae71e0d81a82c9031ea63b19fe
Kraymundo <raymundokyle89@gmail.com>
2025-06-17 00:54:15 -0400

    Cleaning up code.
    Picking this up to start and finish before school starts

--- ZKWhiteCastle.py
+++ ZKWhiteCastle.py
@@ -9,7 +9,6 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("_ZKWhiteCastle")
 pygame.display.set_caption("ZKWhiteCastle")
 
 # Colors
